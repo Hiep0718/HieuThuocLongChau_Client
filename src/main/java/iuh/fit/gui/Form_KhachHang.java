fe54c3a3b7435f8c55a3a31e6ebd68efa3494d55
phanviethieu2004 <hieup5335@gmail.com>
2025-04-21 01:31:14 +0700

    danh sach KH

--- src/main/java/iuh/fit/gui/Form_KhachHang.java
+++ src/main/java/iuh/fit/gui/Form_KhachHang.java
@@ -1,18 +1,26 @@
 package iuh.fit.gui;
 
+import model.KhachHang;
+import services.KhachHangService;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.rmi.Naming;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Form_KhachHang extends JPanel {
 	private JLabel tieuDe;
 	private DefaultTableModel tableModel;
+	private List<KhachHang> dsKH;
 
 	public Form_KhachHang() {
 		initForm();
 	}
 
 	private void initForm() {
+		dsKH = new ArrayList<>();
 		setLayout(new BorderLayout());
 
 		// Tiêu đề
@@ -23,8 +31,9 @@ public class Form_KhachHang extends JPanel {
 		jpTieuDe.add(tieuDe, BorderLayout.CENTER);
 
 		// Bảng hiển thị
-		String[] columns = {"Mã khách hàng", "Tên khách hàng", "Điện thoại", "Email", "Trạng thái"};
+		String[] columns = {"Mã khách hàng", "Tên khách hàng", "Số Điện Thoại", "Email", "Địa chỉ", "Ngày sinh"};
 		tableModel = new DefaultTableModel(columns, 0);
+		updateTableKH();
 		JTable table = new JTable(tableModel);
 		table.getTableHeader().setBackground(new Color(76, 175, 80));
 		table.getTableHeader().setForeground(Color.WHITE);
@@ -33,5 +42,28 @@ public class Form_KhachHang extends JPanel {
 
 		add(jpTieuDe, BorderLayout.NORTH);
 		add(scrollPane, BorderLayout.CENTER);
+	}
+
+	private void updateTableKH()  {
+		// Xóa dữ liệu cũ trong bảng thuốc
+		tableModel.setRowCount(0);
+		try {
+			KhachHangService thuocService = (KhachHangService) Naming.lookup("rmi://localhost:9090/khachHangService");
+			dsKH = thuocService.getAll();
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(this, "Lỗi kết nối đến dịch vụ thuốc: " + e.getMessage(), "Lỗi", JOptionPane.ERROR_MESSAGE);
+		}
+		// Thêm dữ liệu mới vào bảng thuốc
+		for (KhachHang kh : dsKH) {
+			Object[] rowData = {
+					kh.getMaKH(),
+					kh.getTenKH(),
+					kh.getSoDienThoai(),
+					kh.getEmail(),
+					kh.getDiaChi(),
+					kh.getNgaySinh()
+			};
+			tableModel.addRow(rowData);
+		}
 	}
 }
