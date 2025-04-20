0530107b2e23404536fa00222ef3e389de052f38
phanviethieu2004 <hieup5335@gmail.com>
2025-04-21 00:49:38 +0700

    load data tim kiem Nhan Vien

--- src/main/java/iuh/fit/gui/Form_TimNhanVien.java
+++ src/main/java/iuh/fit/gui/Form_TimNhanVien.java
@@ -1,11 +1,19 @@
 package iuh.fit.gui;
 
 
+import model.NhanVien;
+import model.Thuoc;
+import services.NhanVienService;
+import services.ThuocService;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.rmi.Naming;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Form_TimNhanVien extends JPanel{
     // Khai báo các thành phần của form
@@ -15,8 +23,10 @@ public class Form_TimNhanVien extends JPanel{
     private JTable tblKetQua;
     private JScrollPane scrollPane;
 	private DefaultTableModel data;
+    private List<NhanVien> dsNV;
 
     public Form_TimNhanVien() {
+        dsNV = new ArrayList<>();
         // Tạo các thành phần
         lblTimKiem = new JLabel("Nhập mã nhân viên:");
         txtTimKiem = new JTextField(20);
@@ -24,8 +34,9 @@ public class Form_TimNhanVien extends JPanel{
         btnThoat = new JButton("Thoát");
         
         // Dữ liệu mẫu cho bảng kết quả
-        String[] columnNames = {"Mã NV", "Họ Tên","Tuổi","Giới tính", "Điện thoại","Email", "Địa chỉ", "Trạng thái", "Chức vụ"};
+        String[] columnNames = {"Mã NV","Tên nhân viên", "Chú thích","Chức vụ", "Địa chỉ","Email", "Ngày sinh", "Ngày vào làm", "Số điện thoại"};
         data = new DefaultTableModel(columnNames, 0);
+        updateTableNhanVien();
         tblKetQua = new JTable(data);
         
         //Load dữ liệu lên table
@@ -75,6 +86,31 @@ public class Form_TimNhanVien extends JPanel{
 //                dispose();  // Đóng form
             }
         });
+    }
+    private void updateTableNhanVien()  {
+        // Xóa dữ liệu cũ trong bảng thuốc
+        data.setRowCount(0);
+        try {
+            NhanVienService NVService = (NhanVienService) Naming.lookup("rmi://localhost:9090/nhanVienService");
+            dsNV = NVService.getAll();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "Lỗi kết nối đến dịch vụ nhân viên: " + e.getMessage(), "Lỗi", JOptionPane.ERROR_MESSAGE);
+        }
+        // Thêm dữ liệu mới vào bảng thuốc
+        for (NhanVien nv : dsNV) {
+            Object[] rowData = {
+                    nv.getMaNV(),
+                    nv.getTenNV(),
+                    nv.getChuThich(),
+                    nv.getChucVu(),
+                    nv.getDiaChi(),
+                    nv.getEmail(),
+                    nv.getNgaySinh(),
+                    nv.getNgayVaoLam(),
+                    nv.getSoDienThoai()
+            };
+            data.addRow(rowData);
+        }
     }
 
 }
