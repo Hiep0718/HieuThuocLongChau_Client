a22fc5a7a9320c20ec413a75896d5eaa6369fc70
phihungdeptraino2 <hungdz1372004@gmail.com>
2025-04-21 14:40:03 +0700

    Update data table PhieuDatGui

--- src/main/java/iuh/fit/gui/Form_CapNhatPhieuDat.java
+++ src/main/java/iuh/fit/gui/Form_CapNhatPhieuDat.java
@@ -1,9 +1,16 @@
 package iuh.fit.gui;
 
 
+import model.NhanVien;
+import model.PhieuDatThuoc;
+import services.NhanVienService;
+import services.PhieuDatThuocService;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.rmi.Naming;
+import java.util.List;
 
 public class Form_CapNhatPhieuDat extends JPanel{
 	 private JLabel lblTimKiem;
@@ -12,6 +19,7 @@ public class Form_CapNhatPhieuDat extends JPanel{
 	    private JTable tblKetQua;
 	    private JScrollPane scrollPane;
 		private DefaultTableModel data;
+		List<PhieuDatThuoc> list;
 
 	    public Form_CapNhatPhieuDat() {
 			// Tạo các thành phần
@@ -24,7 +32,7 @@ public class Form_CapNhatPhieuDat extends JPanel{
 			String[] columnNames = {"Mã phiếu đặt", "Tên khách hàng ", "ngày đặt", "Điện thoại", "Nhân viên", "Trạng thái",};
 			data = new DefaultTableModel(columnNames, 0);
 			tblKetQua = new JTable(data);
-
+           updateTablePDT();
 
 			scrollPane = new JScrollPane(tblKetQua);
 
@@ -50,4 +58,29 @@ public class Form_CapNhatPhieuDat extends JPanel{
 			add(panelButtons, BorderLayout.SOUTH);
 
 		}
+	private void updateTablePDT()  {
+		// Xóa dữ liệu cũ trong bảng thuốc
+		data.setRowCount(0);
+		try {
+			PhieuDatThuocService  phieuDatThuocService = (PhieuDatThuocService) Naming.lookup("rmi://localhost:9090/phieuDatThuocService");
+			list =phieuDatThuocService.getAll();
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(this, "Lỗi kết nối đến dịch vụ thuốc: " + e.getMessage(), "Lỗi", JOptionPane.ERROR_MESSAGE);
+		}
+		// Thêm dữ liệu mới vào bảng thuốc
+		for (PhieuDatThuoc pdt : list) {
+			Object[] rowData = {
+					pdt.getMaPDT(),
+					pdt.getKhachHang().getTenKH(),
+					pdt.getNgayLapPhieuDat(),
+					pdt.getKhachHang().getSoDienThoai(),
+					pdt.getNhanVien().getTenNV(),
+					pdt.getGhiChu()
+
+
+
+			};
+			data.addRow(rowData);
+		}
+	}
 }
