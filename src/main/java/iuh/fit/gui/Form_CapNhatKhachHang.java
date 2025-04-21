61c7a728da6463cb0798da46c72520e97321a7c2
phanviethieu2004 <hieup5335@gmail.com>
2025-04-21 14:36:07 +0700

    update giao dien KH

--- src/main/java/iuh/fit/gui/Form_CapNhatKhachHang.java
+++ src/main/java/iuh/fit/gui/Form_CapNhatKhachHang.java
@@ -1,15 +1,23 @@
 package iuh.fit.gui;
 
+import model.KhachHang;
+import services.KhachHangService;
+
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.rmi.Naming;
+import java.util.List;
 
 public class Form_CapNhatKhachHang extends JPanel {
     private JTextField txtMaKH, txtTenKH, txtDienThoai, txtEmail;
     private JTable table;
     private DefaultTableModel tableModel;
     private JButton btnThem, btnXoa;
+    private JTextField txtDiaChi, txtNgaySinh;
+    private List<KhachHang> dsKH;
+
 
     public Form_CapNhatKhachHang() {
         // Tiêu đề
@@ -62,6 +70,23 @@ public class Form_CapNhatKhachHang extends JPanel {
         gbc.gridx = 1;
         txtEmail = new JTextField(15);
         pnlInput.add(txtEmail, gbc);
+        // Địa chỉ
+        gbc.weightx = 0.3;
+        gbc.gridx = 0; gbc.gridy = 4;
+        pnlInput.add(new JLabel("Địa chỉ:"), gbc);
+        gbc.weightx = 0.7;
+        gbc.gridx = 1;
+        txtDiaChi = new JTextField(15);
+        pnlInput.add(txtDiaChi, gbc);
+
+// Ngày sinh
+        gbc.weightx = 0.3;
+        gbc.gridx = 0; gbc.gridy = 5;
+        pnlInput.add(new JLabel("Ngày sinh (yyyy-mm-dd):"), gbc);
+        gbc.weightx = 0.7;
+        gbc.gridx = 1;
+        txtNgaySinh = new JTextField(15);
+        pnlInput.add(txtNgaySinh, gbc);
 
         // Các nút chức năng
         btnThem = new JButton("Thêm");
@@ -80,8 +105,9 @@ public class Form_CapNhatKhachHang extends JPanel {
         pnlButton.add(btnXoa);
 
         // Bảng khách hàng
-        String[] columnNames = {"Mã KH", "Tên KH", "Điện Thoại", "Email"};
+        String[] columnNames = {"Mã khách hàng", "Tên khách hàng", "Số Điện Thoại", "Email", "Địa chỉ", "Ngày sinh"};
         tableModel = new DefaultTableModel(columnNames, 0);
+        updateTableKH();
         table = new JTable(tableModel);
         JScrollPane scrollPane = new JScrollPane(table);
 
@@ -95,5 +121,28 @@ public class Form_CapNhatKhachHang extends JPanel {
 
         add(pnlNorth, BorderLayout.NORTH);
         add(scrollPane, BorderLayout.CENTER);
+    }
+
+    private void updateTableKH()  {
+        // Xóa dữ liệu cũ trong bảng thuốc
+        tableModel.setRowCount(0);
+        try {
+            KhachHangService thuocService = (KhachHangService) Naming.lookup("rmi://localhost:9090/khachHangService");
+            dsKH = thuocService.getAll();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "Lỗi kết nối đến dịch vụ thuốc: " + e.getMessage(), "Lỗi", JOptionPane.ERROR_MESSAGE);
+        }
+        // Thêm dữ liệu mới vào bảng thuốc
+        for (KhachHang kh : dsKH) {
+            Object[] rowData = {
+                    kh.getMaKH(),
+                    kh.getTenKH(),
+                    kh.getSoDienThoai(),
+                    kh.getEmail(),
+                    kh.getDiaChi(),
+                    kh.getNgaySinh()
+            };
+            tableModel.addRow(rowData);
+        }
     }
 }
